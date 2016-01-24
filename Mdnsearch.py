c62c64d0d7e76031204dd5f7dcf50862465303fd
unbug <tidelgl@gmail.com>
2016-01-24 21:22:14 +0800

    Create Sbulime Plugin

---
+++ Mdnsearch.py
@@ -0,0 +1,42 @@
+# available commands
+#   codelf_selection
+#   codelf_from_input
+
+
+import sublime
+import sublime_plugin
+
+import subprocess
+import webbrowser
+
+
+def SearchFor(text):
+    url = 'http://unbug.github.io/codelf/#=' + text.replace(' ', '%20')
+    webbrowser.open_new_tab(url)
+
+
+class CodelfSelectionCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for selection in self.view.sel():
+            # if the user didn't select anything, search the currently highlighted word
+            if selection.empty():
+                text = self.view.word(selection)
+
+            text = self.view.substr(selection)
+            SearchFor(text)
+
+
+class CodelfFromInputCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        # Get the search item
+        self.window.show_input_panel('Codelf for', '',
+            self.on_done, self.on_change, self.on_cancel)
+
+    def on_done(self, input):
+        SearchFor(input)
+
+    def on_change(self, input):
+        pass
+
+    def on_cancel(self):
+        pass
