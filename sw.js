691766c33262363bc1346b91d0632f0edb23be49
unbug <tidelgl@gmail.com>
2018-12-21 14:26:34 +0800

    add wasmrocks.com

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@
 // cache, then increment the CACHE_VERSION value. It will kick off the service worker update
 // flow and the old cache(s) will be purged as part of the activate event handler when the
 // updated service worker is activated.
-var CACHE_VERSION = 20181122065617;
+var CACHE_VERSION = 20181221022533;
 var CURRENT_CACHES = {
   prefetch: 'prefetch-cache-v' + CACHE_VERSION
 };
