a5f360df1a2bafcd760f1aa5d0958323765ca8eb
unbug <tidelgl@gmail.com>
2019-01-24 01:02:30 +0800

    fix lint

--- src/components/SearchError.js
+++ src/components/SearchError.js
@@ -11,7 +11,8 @@ export default class SearchError extends React.Component {
       <div className='search-error'>
         <div>Nothing found, please try  <Label color='grey' size='mini'>Quick Search</Label> or come back later :)</div>
         <div>You can also get help from <a href='https://github.com/unbug/codelf/issues'
-                                          target='_blank'>https://github.com/unbug/codelf/issues</a>.
+                                          target='_blank'
+                                          rel="noopener noreferrer" >https://github.com/unbug/codelf/issues</a>.
         </div>
       </div>
     );
