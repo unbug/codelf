6f54d669033081cd46c344764fd16c714161a4ea
unbug <tidelgl@gmail.com>
2019-02-01 19:39:21 +0800

    update denpendence

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "@babel/preset-react"
   ],
   "plugins": [
-    "@babel/plugin-proposal-class-properties"
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-optional-chaining"
   ]
 }
