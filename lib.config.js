deae2b8c6e0695d9adaa648b6aa27bb999947161
unbug <tidelgl@gmail.com>
2019-12-06 15:12:56 +0800

    cache bing translate api

--- lib.config.js
+++ lib.config.js
@@ -23,12 +23,14 @@ exports.serviceWorker = {
   included: [ // cache then network, url must start with it's host
     '"searchcode.com"',
     '"fanyi.youdao.com"',
+    '"api.cognitive.microsofttranslator.com"',
     '"api.github.com"'
   ],
   networkOnly: [ // network falling back to cache, url|path|etc must included in "included"
   ],
   cacheOnly: [ // cache fallback to network, url|path|etc must included in "included"
     '"fanyi.youdao.com"',
+    '"api.cognitive.microsofttranslator.com"',
     '"api.github.com"'
   ],
   excluded: [ // won't be cache, url|path|etc must included in "included"
