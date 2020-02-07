52976643fb223a94a9ab3f812404287b66de30a6
unbug <tidelgl@gmail.com>
2020-02-07 10:53:27 +0800

    fix slider not auto running

--- src/containers/NoticeContainer.js
+++ src/containers/NoticeContainer.js
@@ -12,7 +12,7 @@ export default function NoticeContainer() {
         <Popup position='top center' hoverable={true} positionFixed={true}
           content={
             <div style={{textAlign: 'center'}}>
-              <img src='https://user-images.githubusercontent.com/799578/73824613-67ca5000-4835-11ea-9f75-cd894405a8dd.jpeg' />
+              <img src='https://user-images.githubusercontent.com/799578/73824613-67ca5000-4835-11ea-9f75-cd894405a8dd.jpeg' height='164'/>
               <div>字节跳动职位开放：前端，后端，产品，运营，UX/UI，运维，QA，算法，数据挖掘，实习生等。请扫内推码或点链接</div>
             </div>
           }
@@ -20,10 +20,10 @@ export default function NoticeContainer() {
             <span>[内推] 字节跳动中国，美国，新加坡全职/实习</span>
           } />
       </a>
-      <a className='animated fadeIn' target='_blank' rel='noopener noreferrer'
+      {/* <a className='animated fadeIn' target='_blank' rel='noopener noreferrer'
         href='https://www.yuque.com/?CODELF'>
         [语雀] 阿里技术团队打造的专业云知识库
-      </a>
+      </a> */}
       <a className='animated fadeIn' target='_blank' rel='noopener noreferrer'
         href='https://github.com/unbug/snts'>
         <Icon name='heartbeat' /> SAY NO TO SUICIDE PUBLIC LICENSE
@@ -43,11 +43,18 @@ export default function NoticeContainer() {
 }
 
 function useSliderEffect(el) {
-  const [disable, setDisable] = useState(true);
+  const [disable, setDisable] = useState(false);
   useEffect(() => {
-    const timer = setInterval(() => !disable && renderItem(el.current.children), 15000);
-    return () => clearInterval(timer);
-  }, [disable]); // run an effect and clean it up only once (on mount and unmount), you can pass an empty array ([])
+    let interval = 0;
+    const delay = setTimeout(() => { 
+      interval = setInterval(() => !disable && renderItem(el.current.children), 5000);
+    }, 15000);
+    
+    return () => {
+      clearTimeout(delay);
+      clearInterval(interval);
+    };
+  }, [disable]); 
 
   let active = 1;
   function renderItem(list) {
