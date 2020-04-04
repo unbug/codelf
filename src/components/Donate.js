d583f25d299bb4feb7ebcf7bbcd5da775788351f
unbug <tidelgl@gmail.com>
2020-04-04 17:52:01 +0800

    update donate text

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import * as Tools from '../utils/Tools';
 
-const cnText = Tools.randomList(['颈椎病晚期', '穷困潦倒', '快交不起房租', '天天加班', '有可能会猝死'], 1)[0];
 export default function Donate(props) {
+  const cnText = Tools.randomList(['颈椎病晚期', '穷困潦倒', '快交不起房租', '天天加班', '有可能会猝死'], 1)[0];
   let text = <h4 className='lang'>Buy <a href='https://twitter.com/unbug' rel='noopener noreferrer' target='_blank'>@unbug</a> a drink</h4>;
   if (props.isZH) {
     text = <h4 className='lang cn'>给<a href='https://weibo.com/unbug/' rel='noopener noreferrer' target='_blank'>{cnText}的作者</a>打赏个红包吧</h4>;
